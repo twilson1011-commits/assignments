1062e464cfe4610f5957eeb1b508464bebdf331b
Tyler Wilson <twilson1011@my.msutexas.edu>
2026-03-26 21:18:48 -0500

    Refactor argument parsing and error handling

--- Po2/Args.cpp
+++ Po2/Args.cpp
@@ -1,136 +1,130 @@
 #include "Args.h"
-#include <stdexcept>
-#include <string>
 #include <iostream>
+#include <stdexcept>
 
-using namespace std;
+static void usage() {
+    std::cerr << "Usage: ./imgtool <input_image> <output_image> [options]\n";
+}
 
-static bool is_option(const string& s) {
+static void error(const std::string& msg) {
+    std::cerr << "Error: " << msg << "\n";
+    usage();
+    throw std::runtime_error(msg);
+}
+
+static bool is_option(const std::string& s) {
     return !s.empty() && s[0] == '-';
 }
 
-static bool parse_int(const string& s, int& value) {
+static int parse_int(const std::string& s, const std::string& opt) {
     try {
         size_t idx;
-        value = stoi(s, &idx);
-        return idx == s.size();
+        int val = std::stoi(s, &idx);
+        if (idx != s.size()) throw std::invalid_argument("extra chars");
+        return val;
     } catch (...) {
-        return false;
+        error("invalid integer for " + opt + ": " + s);
     }
+    return 0;
 }
 
-static void validate_brighten(int v) {
-    if (v < -255 || v > 255)
-        throw runtime_error("brighten must be in [-255, 255]");
+static void validate_brighten(int val) {
+    if (val < -255 || val > 255)
+        error("brighten must be in [-255, 255]");
 }
 
-static void validate_rotate(int v) {
-    if (!(v == 0 || v == 90 || v == 180 || v == 270))
-        throw runtime_error("rotate must be one of {0, 90, 180, 270}");
+static void validate_rotate(int val) {
+    if (!(val == 0 || val == 90 || val == 180 || val == 270))
+        error("rotate must be one of {0, 90, 180, 270}");
 }
 
 Args Args::parse(int argc, char* argv[]) {
-
-    if (argc < 3)
-        throw runtime_error("missing output file");
+    if (argc < 3) {
+        error("missing required arguments");
+    }
 
     Args args;
-
     args.input = argv[1];
     args.output = argv[2];
 
     for (int i = 3; i < argc; ++i) {
+        std::string token = argv[i];
 
-        string token = argv[i];
+        // Handle --option=value
+        std::string opt = token;
+        std::string val;
 
-        if (token == "--grayscale" || token == "-g") {
+        size_t eq = token.find('=');
+        if (eq != std::string::npos) {
+            opt = token.substr(0, eq);
+            val = token.substr(eq + 1);
+        }
+
+        // -------- FLAGS --------
+        if (opt == "--grayscale" || opt == "-g") {
             args.grayscale = true;
         }
-
-        else if (token == "--blur" || token == "-l") {
+        else if (opt == "--blur" || opt == "-l") {
             args.blur = true;
         }
-
-        else if (token == "--flipH" || token == "-h") {
+        else if (opt == "--flipH" || opt == "-h") {
             args.flipH = true;
         }
-
-        else if (token == "--flipV" || token == "-v") {
+        else if (opt == "--flipV" || opt == "-v") {
             args.flipV = true;
         }
 
-        else if (token.rfind("--brighten=",0) == 0) {
+        // -------- BRIGHTEN --------
+        else if (opt == "--brighten" || opt == "-b") {
+            if (val.empty()) {
+                if (i + 1 >= argc)
+                    error("--brighten requires an integer value");
 
-            string val = token.substr(11);
-            int num;
+                val = argv[++i];
+                if (is_option(val))
+                    error("--brighten expected a value but got option " + val);
+            }
 
-            if (!parse_int(val,num))
-                throw runtime_error("invalid integer for --brighten: " + val);
-
+            int num = parse_int(val, "--brighten");
             validate_brighten(num);
 
             args.use_brighten = true;
             args.brighten = num;
         }
 
-        else if (token == "--brighten" || token == "-b") {
+        // -------- ROTATE --------
+        else if (opt == "--rotate" || opt == "-r") {
+            if (val.empty()) {
+                if (i + 1 >= argc)
+                    error("--rotate requires an integer value");
 
-            if (i+1 >= argc)
-                throw runtime_error("--brighten requires an integer value");
+                val = argv[++i];
+                if (is_option(val))
+                    error("--rotate expected a value but got option " + val);
+            }
 
-            string val = argv[++i];
-
-            if (is_option(val))
-                throw runtime_error("--brighten expected a value but got option " + val);
-
-            int num;
-
-            if (!parse_int(val,num))
-                throw runtime_error("invalid integer for --brighten: " + val);
-
-            validate_brighten(num);
-
-            args.use_brighten = true;
-            args.brighten = num;
-        }
-
-        else if (token.rfind("--rotate=",0) == 0) {
-
-            string val = token.substr(9);
-            int num;
-
-            if (!parse_int(val,num))
-                throw runtime_error("invalid integer for --rotate: " + val);
-
+            int num = parse_int(val, "--rotate");
             validate_rotate(num);
 
             args.use_rotate = true;
             args.rotate = num;
         }
 
-        else if (token == "--rotate" || token == "-r") {
-
-            if (i+1 >= argc)
-                throw runtime_error("--rotate requires an integer value");
-
-            string val = argv[++i];
-
-            if (is_option(val))
-                throw runtime_error("--rotate expected a value but got option " + val);
-
-            int num;
-
-            if (!parse_int(val,num))
-                throw runtime_error("invalid integer for --rotate: " + val);
-
-            validate_rotate(num);
-
-            args.use_rotate = true;
-            args.rotate = num;
+        // -------- GROUPED SHORT FLAGS (EXTRA CREDIT) --------
+        else if (token.size() > 2 && token[0] == '-' && token[1] != '-') {
+            for (size_t j = 1; j < token.size(); ++j) {
+                char c = token[j];
+                if (c == 'g') args.grayscale = true;
+                else if (c == 'l') args.blur = true;
+                else if (c == 'h') args.flipH = true;
+                else if (c == 'v') args.flipV = true;
+                else error(std::string("unknown option -") + c);
+            }
         }
 
+    
         else {
-            throw runtime_error("unknown option " + token);
+            error("unknown option " + token);
         }
     }
 
