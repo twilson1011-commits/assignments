8d761917fb0073f024d10e94fec81408f8599958
Tyler Wilson <twilson1011@my.msutexas.edu>
2026-04-22 00:52:17 +0000

    created fliph

--- P03/src/FlipH.cpp
+++ P03/src/FlipH.cpp
@@ -6,14 +6,11 @@ std::string FlipH::name() const {
 }
 
 void FlipH::apply(Grid& pixels) {
-    (void)pixels;  // remove this line when implementing
-    // TODO: iterate over every row and reverse it left-to-right.
-    //
-    // Hint: for each row, swap pixels at mirror positions:
-    //   for (auto& row : pixels) {
-    //       int width = row.size();
-    //       for (int col = 0; col < width / 2; ++col) {
-    //           std::swap(row[col], row[width - 1 - col]);
-    //       }
-    //   }
+     
+       for (auto& row : pixels) {
+           int width = row.size();
+           for (int col = 0; col < width / 2; ++col) {
+               std::swap(row[col], row[width - 1 - col]);
+           }
+       }
 }
