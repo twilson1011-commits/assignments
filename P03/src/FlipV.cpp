c25470de9093fb0c3b2b0b07ea0445ed98595955
Tyler Wilson <twilson1011@my.msutexas.edu>
2026-04-22 00:53:31 +0000

    made flipv

--- P03/src/FlipV.cpp
+++ P03/src/FlipV.cpp
@@ -6,12 +6,9 @@ std::string FlipV::name() const {
 }
 
 void FlipV::apply(Grid& pixels) {
-    (void)pixels;  // remove this line when implementing
-    // TODO: swap rows from the outside in.
-    //
-    // Hint:
-    //   int height = pixels.size();
-    //   for (int row = 0; row < height / 2; ++row) {
-    //       std::swap(pixels[row], pixels[height - 1 - row]);
-    //   }
+    
+       int height = pixels.size();
+       for (int row = 0; row < height / 2; ++row) {
+          std::swap(pixels[row], pixels[height - 1 - row]);
+       }
 }
